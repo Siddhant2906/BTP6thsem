99d0cedc948d86bc9e6d7b46bd95fc8bf740a0b5
ashu26jha <ashutosh26jha@gmail.com>
2024-04-29 03:44:44 +0530

    Write contract done ✅

--- Frontend/src/app/Screens/Test.tsx
+++ Frontend/src/app/Screens/Test.tsx
@@ -1,24 +1,49 @@
 "use client"
-import useTest from '../../../contractReader';
+import contractReader from '../../../contractReader';
 import {
     useAccount,
+    useWriteContract,
 } from 'wagmi'
+import contractWrite from '../../../writeContract';
 
 
 export default function MyComponent() {
+
+    const {
+        writeContract
+    } = useWriteContract()
 
     const { address } = useAccount()
     console.log(address)
 
     const handleClick = () => {
-        useTest('getOwner', []);
-        useTest('getCurrentTokenID', []);
-        useTest('getList', [0])
+        contractReader('getOwner', []);
+        contractReader('getCurrentTokenID', []);
+        contractReader('getList', [0]);
     };
 
+    async function addFile (){
+        contractWrite('addFile',['abc'], writeContract);
+    }
+
+    async function editFile () {
+        contractWrite('editFile',['def',BigInt('0')], writeContract)
+    }
+
     return (
-        <div className="mt-16">
-            <button onClick={handleClick}>Read All data</button>
+        <div>
+            <div className="mt-16 ">
+                <button onClick={handleClick}>Read All data</button>
+            </div>
+
+            <div className="mt-16 ">
+                <button onClick={addFile}>Add File</button>
+            </div>
+
+            <div className="mt-16 ">
+                <button onClick={editFile}>Edit File</button>
+            </div>
         </div>
+
     );
 }
